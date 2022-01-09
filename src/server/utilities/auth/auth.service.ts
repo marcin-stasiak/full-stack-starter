cb4f45760605e75eb9fb25d4e8008d0d2618fccf
Marcin Stasiak <kontakt@marcinstasiak.pl>
2022-01-09 22:39:31 +0100

    auth

--- src/server/utilities/auth/auth.service.ts
+++ src/server/utilities/auth/auth.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+import { UsersService } from '../../endpoints/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService
+  ) {}
+
+  async validateUser(username: string, pass: string): Promise<any> {
+  }
+  }
