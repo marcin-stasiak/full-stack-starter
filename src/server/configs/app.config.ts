b5c4946ad6b946d48cfa5439a92f4400fd0dc6c1
Marcin Stasiak <kontakt@marcinstasiak.pl>
2022-02-09 08:48:33 +0100

    rename project config

--- src/server/configs/app.config.ts
+++ src/server/configs/app.config.ts
@@ -1,6 +1,6 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('app', () => ({
-  name: process.env.APP_NAME || 'Starter',
+  name: process.env.APP_NAME || 'Full Stack Starter',
   secret: process.env.APP_SECRET || 'secret',
 }));
