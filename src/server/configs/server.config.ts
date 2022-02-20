737b5b1fe194914312d172361ce3ad5e603874cf
Marcin Stasiak <kontakt@marcinstasiak.pl>
2022-02-20 10:32:35 +0100

    production config fix

--- src/server/configs/server.config.ts
+++ src/server/configs/server.config.ts
@@ -1,4 +1,4 @@
 export default () => ({
-  development: process.env.NODE_ENV !== 'production' || true,
+  development: process.env.NODE_ENV !== 'production',
   port: Number.parseInt(process.env.PORT, 10) || 3000,
 });
