bd2fd89754ddad26ac92e1d2d800603445ca50ef
Marcin Stasiak <kontakt@marcinstasiak.pl>
2022-02-25 11:50:07 +0100

    update

--- src/client/next.config.js
+++ src/client/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+require('dotenv').config({ path: '../../.env' });
+
 module.exports = {
   distDir: '../../.next',
   reactStrictMode: true,
   swcMinify: false,
+  env: {
+    NEXT_PUBLIC_URI_ORIGIN: process.env.NEXT_PUBLIC_URI_ORIGIN,
+  },
 };
